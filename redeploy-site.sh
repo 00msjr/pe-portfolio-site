795d1a06212dd8cef8ab1c457a474f00e17cf438
michael <mikey@mbam2.local>
2025-07-12 11:32:02 -0400

    edit redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,9 +3,9 @@
 tmux kill-server 2>/dev/null
 cd ~/pe-portfolio-site || exit 1
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
+source python3-venv/bin/activate
 pip install -r requirements.txt
 tmux new-session -d -s flask-server -c ~/pe-portfolio-site \; \
-    send-keys 'source python3-virtualenv/bin/activate' Enter \; \
-    send-keys 'export FLASK_ENV=development' Enter \; \
-    send-keys 'flask run --host=0.0.0.0 --port=5000' Enter
+  send-keys 'source python3-venv/bin/activate' Enter \; \
+  send-keys 'export FLASK_ENV=development' Enter \; \
+  send-keys 'flask run --host=0.0.0.0 --port=5000' Enter
