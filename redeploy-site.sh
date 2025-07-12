1b1acc1332803760c2b9bd17e1f3cc6a8375ad37
michael <mikey@mbam2.local>
2025-07-12 11:49:42 -0400

    fix redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,6 +3,8 @@
 tmux kill-server 2>/dev/null
 cd ~/pe-portfolio-site || exit 1
 git fetch && git reset origin/main --hard
+rm -rf python3-venv
+python3 -m venv python3-venv
 source python3-venv/bin/activate
 pip install -r requirements.txt
 tmux new-session -d -s flask-server -c ~/pe-portfolio-site \; \
