67ef560c271b405c84e99b46e2781b809eb16cc3
michael <mikey@mbam2.local>
2025-07-30 15:51:26 -0400

    Fix duplicate database configuration in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -20,14 +20,6 @@ else:
         port=3306,
     )
 
-mydb = MySQLDatabase(
-    os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
-    port=3306,
-)
-
 
 class TimelinePost(Model):
     name = CharField()
